a82daabb1b5534bd96196edd00280165d04a91f9
Roiney <nei_beal@hotmail.com>
2025-11-01 11:12:57 -0300

    start project

---
+++ src/main/java/com/example/routing_backend/RoutingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.routing_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutingBackendApplication.class, args);
+	}
+
+}
